bc83181fa027b01db1c771c449dbbd10a024a7ad
Michal Altair Valasek <michal.valasek@altairis.cz>
2017-05-23 13:50:07 +0200

    Added pickup mail folder service w/ tests

---
+++ Altairis.Services.Mailing.Rfc2822.Tests/PickupFolderMailServiceTests.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Altairis.Services.Mailing.Rfc2822.Tests {
+    public class PickupFolderMailServiceTests {
+
+        [Fact]
+        public async Task SendPlainTextMail_Test() {
+            var mx = new PickupFolderMailService(CreateTempFolder("plain"));
+            var msg = new MailMessageDto {
+                From = new MailAddressDto("sender@example.com", "Example Sender"),
+                Subject = "luouèkı kùò úpìl ïábelské ódy - subject",
+                BodyText = "luouèkı kùò úpìl ïábelské ódy - text."
+            };
+            msg.To.Add(new MailAddressDto("recipient@example.com", "Example Recipient"));
+            await mx.SendMessageAsync(msg);
+
+            Assert.True(EmlFileExists(mx.PickupFolderName));
+        }
+
+        [Fact]
+        public async Task SendHtmlMail_Test() {
+            var mx = new PickupFolderMailService(CreateTempFolder("html"));
+            var msg = new MailMessageDto {
+                From = new MailAddressDto("sender@example.com", "Example Sender"),
+                Subject = "luouèkı kùò úpìl ïábelské ódy - subject",
+                BodyHtml = "<html><body><p>luouèkı kùò úpìl ïábelské ódy <b>v HTML</b>.</p></body></html>"
+            };
+            msg.To.Add(new MailAddressDto("recipient@example.com", "Example Recipient"));
+            await mx.SendMessageAsync(msg);
+
+            Assert.True(EmlFileExists(mx.PickupFolderName));
+        }
+
+        [Fact]
+        public async Task SendAlternateMail_Test() {
+            var mx = new PickupFolderMailService(CreateTempFolder("alternate"));
+            var msg = new MailMessageDto {
+                From = new MailAddressDto("sender@example.com", "Example Sender"),
+                Subject = "luouèkı kùò úpìl ïábelské ódy - subject",
+                BodyText = "luouèkı kùò úpìl ïábelské ódy - text.",
+                BodyHtml = "<html><body><p>luouèkı kùò úpìl ïábelské ódy <b>v HTML</b>.</p></body></html>"
+            };
+            msg.To.Add(new MailAddressDto("recipient@example.com", "Example Recipient"));
+            await mx.SendMessageAsync(msg);
+
+            Assert.True(EmlFileExists(mx.PickupFolderName));
+        }
+
+        [Fact]
+        public async Task SendMailWithAttachment_Test() {
+            var mx = new PickupFolderMailService(CreateTempFolder("attachment"));
+            var msg = new MailMessageDto {
+                From = new MailAddressDto("sender@example.com", "Example Sender"),
+                Subject = "luouèkı kùò úpìl ïábelské ódy - subject",
+                BodyText = "luouèkı kùò úpìl ïábelské ódy - text.",
+                BodyHtml = "<html><body><p>luouèkı kùò úpìl ïábelské ódy <b>v HTML</b>.</p></body></html>"
+            };
+            msg.To.Add(new MailAddressDto("recipient@example.com", "Example Recipient"));
+
+            using (var ms = new MemoryStream(System.Text.Encoding.UTF8.GetBytes("Test attachment file"))) {
+                msg.Attachments.Add(new AttachmentDto { Name = "attachment.txt", MimeType = "text/plain", Stream = ms });
+                await mx.SendMessageAsync(msg);
+            }
+
+            Assert.True(EmlFileExists(mx.PickupFolderName));
+        }
+
+        private static bool EmlFileExists(string folderName) {
+            return Directory.EnumerateFiles(folderName, "*.eml").Count() == 1;
+        }
+
+        private static string CreateTempFolder(string suffix) {
+            var folderName = Path.Combine(Path.GetTempPath(), "PickupFolderMailServiceTest", DateTime.Now.ToString("yyyyMMdd-HHmmss-fffffff") + "-" + suffix);
+            Directory.CreateDirectory(folderName);
+            return folderName;
+        }
+
+    }
+}
---
+++ Altairis.Services.Mailing.Rfc2822/PickupFolderMailService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Linq;
+
+namespace Altairis.Services.Mailing.Rfc2822 {
+    public class PickupFolderMailService : IMailerService {
+
+        private readonly Func<string> getTempFileName;
+
+        public string PickupFolderName { get; }
+
+        public PickupFolderMailService(string pickupFolderName, Func<string> getTempFileName = null) {
+            if (pickupFolderName == null) throw new ArgumentNullException(nameof(pickupFolderName));
+            if (string.IsNullOrWhiteSpace(pickupFolderName)) throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(pickupFolderName));
+            if (!Directory.Exists(pickupFolderName)) throw new DirectoryNotFoundException();
+
+            this.PickupFolderName = pickupFolderName;
+            this.getTempFileName = getTempFileName ?? Path.GetTempFileName;
+        }
+
+        public async Task SendMessageAsync(MailMessageDto message) {
+            if (message == null) throw new ArgumentNullException(nameof(message));
+
+            // Convert to message
+            var msg = message.ToMimeMessage();
+
+            // Write to temp file to avoid pickup of incomplete message
+            var tempFileName = this.getTempFileName();
+            using (var sw = File.CreateText(tempFileName)) {
+                // Write envelope sender
+                await sw.WriteLineAsync($"X-Sender: <{message.From.Address}>");
+
+                // Write envelope receivers
+                var receivers = message.To
+                    .Union(message.Cc)
+                    .Union(message.Bcc);
+                foreach (var item in receivers.Select(x => x.Address)) {
+                    await sw.WriteLineAsync($"X-Receiver: <{item}>");
+                }
+
+                // Flush data and write rest of message
+                await sw.FlushAsync();
+                msg.WriteTo(sw.BaseStream);
+            }
+
+            // Move the file to final destination
+            var msgFileName = Path.Combine(this.PickupFolderName, string.Join(".", DateTime.Now.ToString("yyyyMMdd-HHmmss"), Guid.NewGuid().ToString("N"), "eml"));
+            File.Move(tempFileName, msgFileName);
+        }
+    }
+}
