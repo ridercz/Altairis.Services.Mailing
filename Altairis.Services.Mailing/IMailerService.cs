9e8609ec20b26d6e123186d393f050dc31dc22ad
Michal Altair Valasek <michal.valasek@altairis.cz>
2017-05-23 13:23:15 +0200

    Renamed to Async

--- Altairis.Services.Mailing/IMailerService.cs
+++ Altairis.Services.Mailing/IMailerService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace Altairis.Services.Mailing {
     public interface IMailerService {
 
-        Task SendMessage(MailMessageDto message);
+        Task SendMessageAsync(MailMessageDto message);
 
     }
 }
