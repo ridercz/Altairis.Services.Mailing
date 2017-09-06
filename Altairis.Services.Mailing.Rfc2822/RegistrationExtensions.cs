84f7513a3a69d64043f06bc9e40f74589d8f2f24
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-09-06 05:01:51 +0200

    Fixed name of registration methods.

--- Altairis.Services.Mailing.Rfc2822/RegistrationExtensions.cs
+++ Altairis.Services.Mailing.Rfc2822/RegistrationExtensions.cs
@@ -7,12 +7,12 @@ using System.Text;
 namespace Microsoft.Extensions.DependencyInjection {
     public static class RegistrationExtensions {
 
-        public static IServiceCollection AddPickupFolderMailService(this IServiceCollection services, PickupFolderMailerServiceOptions options) {
+        public static IServiceCollection AddPickupFolderMailerService(this IServiceCollection services, PickupFolderMailerServiceOptions options) {
             services.AddSingleton<IMailerService>(new PickupFolderMailerService(options));
             return services;
         }
 
-        public static IServiceCollection AddPickupFolderMailService(this IServiceCollection services, string pickupFolderName) {
+        public static IServiceCollection AddPickupFolderMailerService(this IServiceCollection services, string pickupFolderName) {
             if (pickupFolderName == null) throw new ArgumentNullException(nameof(pickupFolderName));
             if (string.IsNullOrWhiteSpace(pickupFolderName)) throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(pickupFolderName));
 
@@ -21,7 +21,7 @@ namespace Microsoft.Extensions.DependencyInjection {
             return services;
         }
 
-        public static IServiceCollection AddSmtpServerMailService(this IServiceCollection services, SmtpServerMailerServiceOptions options) {
+        public static IServiceCollection AddSmtpServerMailerService(this IServiceCollection services, SmtpServerMailerServiceOptions options) {
             services.AddSingleton<IMailerService>(new SmtpServerMailerService(options));
             return services;
         }
