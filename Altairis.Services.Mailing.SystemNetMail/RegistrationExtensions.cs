a72461dea103aed08b089235e1cde0aacecc9cde
Michal Altair Valášek <michal.valasek@altairis.cz>
2017-11-02 14:08:32 +0100

    Added registration extensions

---
+++ Altairis.Services.Mailing.SystemNetMail/RegistrationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Altairis.Services.Mailing;
+using Altairis.Services.Mailing.SystemNetMail;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection {
+    public static class RegistrationExtensions {
+
+        public static IServiceCollection AddPickupFolderMailerService(this IServiceCollection services, PickupFolderMailerServiceOptions options) {
+            services.AddSingleton<IMailerService>(new PickupFolderMailerService(options));
+            return services;
+        }
+
+        public static IServiceCollection AddPickupFolderMailerService(this IServiceCollection services, string pickupFolderName) {
+            if (pickupFolderName == null) throw new ArgumentNullException(nameof(pickupFolderName));
+            if (string.IsNullOrWhiteSpace(pickupFolderName)) throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(pickupFolderName));
+
+            var options = new PickupFolderMailerServiceOptions { PickupFolderName = pickupFolderName };
+            services.AddSingleton<IMailerService>(new PickupFolderMailerService(options));
+            return services;
+        }
+
+    }
+}
