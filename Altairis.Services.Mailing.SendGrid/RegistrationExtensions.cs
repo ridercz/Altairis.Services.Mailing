8b70c6040ec6766e35a2189e2d63aeb9ad51a915
Michal Altair Valášek <michal.valasek@altairis.cz>
2018-12-11 15:06:15 +0100

    Added DI registration extensions for SendGrid

---
+++ Altairis.Services.Mailing.SendGrid/RegistrationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Altairis.Services.Mailing;
+using Altairis.Services.Mailing.SendGrid;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection {
+    public static class AltairisServicesMailingSendGridRegistrationExtensions {
+
+        public static IServiceCollection AddPickupFolderMailerService(this IServiceCollection services, SendGridMailerServiceOptions options) {
+            services.AddSingleton<IMailerService>(new SendGridMailerService(options));
+            return services;
+        }
+
+        public static IServiceCollection AddPickupFolderMailerService(this IServiceCollection services, string apiKey) {
+            if (apiKey == null) throw new ArgumentNullException(nameof(apiKey));
+            if (string.IsNullOrWhiteSpace(apiKey)) throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(apiKey));
+
+            var options = new SendGridMailerServiceOptions { ApiKey = apiKey };
+            services.AddSingleton<IMailerService>(new SendGridMailerService(options));
+            return services;
+        }
+
+    }
+}
