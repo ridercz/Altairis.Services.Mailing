90753bdc9bb4f1e2d30e308779051c93b4067c77
Michal Altair Valášek <michal.valasek@altairis.cz>
2020-04-30 20:57:51 +0200

    Added registration extensions

---
+++ Altairis.Services.Mailing.Mandrill/RegistrationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Altairis.Services.Mailing;
+using Altairis.Services.Mailing.Mandrill;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection {
+    public static class AltairisServicesMailingMandrillRegistrationExtensions {
+
+        public static IServiceCollection AddMandrillMailerService(this IServiceCollection services, MandrillMailerServiceOptions options) {
+            services.AddSingleton<IMailerService>(new MandrillMailerService(options));
+            return services;
+        }
+
+        public static IServiceCollection AddMandrillMailerService(this IServiceCollection services, string apiKey) {
+            if (apiKey == null) throw new ArgumentNullException(nameof(apiKey));
+            if (string.IsNullOrWhiteSpace(apiKey)) throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(apiKey));
+
+            var options = new MandrillMailerServiceOptions { ApiKey = apiKey };
+            services.AddSingleton<IMailerService>(new MandrillMailerService(options));
+            return services;
+        }
+
+    }
+}
