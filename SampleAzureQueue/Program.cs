7206e582ebf4c52353f0bc82ab67d45d8c5f5098
Michal Altair Valasek <michal.valasek@altairis.cz>
2026-08-13 19:17:41 +0200

    Add SAS to demo

--- SampleAzureQueue/Program.cs
+++ SampleAzureQueue/Program.cs
@@ -1,6 +1,7 @@
 using Altairis.Services.Mailing;
 using Altairis.Services.Mailing.AzureQueue;
 using Altairis.Services.Mailing.SystemNetMail;
+using Azure.Storage.Queues;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +11,15 @@ builder.Services.AddRazorPages();
 // Register configuration object for both parts of the Azure Queue mailer service (sender and receiver)
 builder.Services.AddSingleton(new AzureQueueMailerServiceOptions {
     DefaultFrom = new("from@example.com", "Example From"),
-    ConnectionString = builder.Configuration.GetConnectionString("MailingQueue"),
-    QueueName = "mailing",
-    ContainerName = "mailing",
+
+    // Use either this set of options to connect to the Azure Storage using a connection string
+    //ConnectionString = builder.Configuration.GetConnectionString("MailingQueue"),
+    //QueueName = "mailing",
+    //ContainerName = "mailing",
+    
+    // Or use this set of options to connect to the Azure Storage using a SAS token
+    ContainerSasUriFactory = GetContainerSasToken,
+    QueueSasUriFactory = GetQueueSasToken
 });
 
 // Register the Azure Queue mailer service that will send messages to the Azure Queue
@@ -29,6 +36,20 @@ builder.Services.AddKeyedSingleton<IMailerService>(nameof(AzureQueueReceiverServ
 
 // Build the application, configure the request pipeline and run it
 var app = builder.Build();
-app.MapStaticAssets();
 app.MapRazorPages();
 app.Run();
+
+// In real application, you would probably get the SAS token from a secure service, but for this sample, we will generate it on the fly
+async Task<Uri> GetQueueSasToken() { 
+    var queueClient = new QueueClient(builder.Configuration.GetConnectionString("MailingQueue"), "mailingsas");
+    await queueClient.CreateIfNotExistsAsync();
+    var sasUri = queueClient.GenerateSasUri(Azure.Storage.Sas.QueueSasPermissions.All, DateTimeOffset.UtcNow.AddSeconds(90));
+    return sasUri;
+}
+
+async Task<Uri> GetContainerSasToken() {
+    var containerClient = new Azure.Storage.Blobs.BlobContainerClient(builder.Configuration.GetConnectionString("MailingQueue"), "mailingsas");
+    await containerClient.CreateIfNotExistsAsync();
+    var sasUri = containerClient.GenerateSasUri(Azure.Storage.Sas.BlobContainerSasPermissions.All, DateTimeOffset.UtcNow.AddSeconds(90));
+    return sasUri;
+}
